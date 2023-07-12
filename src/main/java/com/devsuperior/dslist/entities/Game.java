62e41ee3db0ec9e74783a42eb6f9294678d12723
Ismael Ramos <ismael.vhs@gmail.com>
2023-07-12 02:29:08 -0300

    Configuração da classe com persistencia

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -2,9 +2,22 @@ package com.devsuperior.dslist.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
 public class Game {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String plataforms;
